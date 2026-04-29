cb187a1c5bb8847e85b15e4b127e94815b14c186
Your Name <CARYUlra>
2026-04-29 13:00:06 -0300

    banana

--- 12-14-16-18-20/script.js
+++ 12-14-16-18-20/script.js
@@ -5,10 +5,16 @@ let fim = prompt("fale o fim");
 if (isNaN(inicio) || isNaN(fim)) {
     alert("digite numeralios");
 } else {
+    if (inicio > fim){
+        pedro = inicio
+        inicio = [fim]
+        fim = [pedro]
+    }  
     for (let contador = inicio; contador <= fim; contador++) {
         if (contador % 2 === 0) {
             banana += contador + " ";
         }
     }
     alert(banana);
+
 }
